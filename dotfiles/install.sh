605bbd30dfaa3244d50cd5b9530731edacbc71b4
kujonello79 <96420570+kujonello79@users.noreply.github.com>
2021-12-21 17:00:53 +0100

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -13,16 +13,16 @@ echo
 echo ---------------------------------------------------------------------------------------------------------
 echo
 echo
-#
-#while true; do
-#	    read -p "Do you wish to proceed? y/n " yn
-#	        case $yn in
-#			        [Yy]* ) make install; break;;
-#				        [Nn]* ) exit;;
-#					        * ) echo "Please answer yes or no.";;
-#						    esac
-#					    done
-#
+
+while true; do
+	    read -p "Do you wish to proceed? y/n " yn
+	        case $yn in
+			        [Yy]* ) make install; break;;
+				        [Nn]* ) exit;;
+					        * ) echo "Please answer yes or no.";;
+						    esac
+					    done
+
 #Yes/No yoinked from stackoverflow
 
 mkdir ~/.config
@@ -36,7 +36,53 @@ yay -Sy
 
 #Updating repositories
 
-yay -Syy pipewire-alsa pipewire pipewire-pulse pipewire-jack bspwm alacritty cava cmatrix htop treefetch librewolf pcmanfm picom polybar sxhkd xorg xorg-xinit dmenu feh all-repository-fonts nerd-fonts-complete
+                        read -p "Do you want to install pipewire(1) or pulseaudio(2)? 1/2 " AUDIO
+                            case $AUDIO in
+                                        1 ) yay -S pipewire pipewire-pulse; break;;
+                                        2 ) yay -S pulseaudio; break;;
+                                        * ) echo "Please answer 1/2.";;
+                                                            esac
+                                                    done
+
+#Pipewire/Pulse
+
+                        read -p "Do you want to install terminal application like cmatrix?(3), no?(4) " STUFF
+                            case $STUFF in
+                                        3 ) yay -S cava cmatrix htop ; break;;
+                                        4 ) echo ok; break;;
+                                        * ) echo "Please answer 3/4.";;
+                                                            esac
+                                                    done
+
+#Cmatrix
+
+                        read -p "Which File Manager would you like to install? pcmanfm (5), nautilus (6), dolphin (7), ranger (8), fff (9), nemo (10) or none (11)" FM
+                            case $FM in
+                                        5 ) yay -S pcmanfm ; break;;
+                                        6 ) yay -S nautilus; break;;
+                                        7 ) yay -S dolphin; break;;
+                                        8 ) yay -S ranger; break;;
+                                        9 ) yay -S fff; break;;
+                                        10 ) yay -S nemo; break;;
+                                        11 ) echo ok; break;;
+                                        * ) echo "Please answer 5/6/7/8/9/10/11";;
+                                                            esac
+                                                    done
+#FileManager
+
+                        read -p "Which terminal emulator would you like to install? alacritty (12), st (13), kitty (14), xfce4-termianl (15), none (16)" TERMINAL
+                            case $TERMINAL in
+                                        12 ) yay -S alacritty; break;;
+                                        13 ) yay -S st; break;;
+                                        14 ) yay -S kitty; break;;
+                                        15 ) yay -S xfce4-terminal; break;;
+                                        16 ) echo ok; break;;
+                                        * ) echo "Please answer 12/13/14/15/16";;
+                                                            esac
+                                                    done
+
+echo Installing window manager, app launcher, xorg etc.
+yay -Syy bspwm picom polybar sxhkd xorg xorg-xinit dmenu feh nerd-fonts-complete
 
 
 #Installing required packages
