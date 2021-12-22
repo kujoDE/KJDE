0ea26116c30d3fd3060121a9aaf68723cf3dd83f
kujonello79 <96420570+kujonello79@users.noreply.github.com>
2021-12-22 12:43:42 +0000

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -25,7 +25,25 @@ while true; do
 						    esac
 					    done
 
-if which yay | grep -q '/yay'; then
+if which doas | grep -q '/doas'; then
+sudo pacman -S --noconfirm base-devel
+sudo pacman -S --noconfirm git
+cd git
+sudo git clone https://aur.archlinux.org/yay.git
+sudo chown -R $USER:$USER ./yay
+cd yay
+makepkg -si
+fi
+
+if which sudo | grep -q '/sudo'; then
+doas pacman -S --noconfirm base-devel
+doas pacman -S --noconfirm git
+cd git
+doas git clone https://aur.archlinux.org/yay.git
+doas chown -R $USER:$USER ./yay
+cd yay
+makepkg -si
+fi
 
 #Yes/No yoinked from stackoverflow
 mkdir ~/.config
