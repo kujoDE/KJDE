7491ea051835f76ce385259bdf04a55063b95789
kujonello79 <96420570+kujonello79@users.noreply.github.com>
2021-12-23 09:20:49 +0000

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -28,6 +28,7 @@ while true; do
 
 sudo pacman -S --noconfirm base-devel
 doas pacman -S --noconfirm base-devel
+doas pacman -R --noconfirm sudo
 sudo pacman -S --noconfirm git
 doas pacman -S --noconfirm git
 cd git
@@ -46,14 +47,14 @@ mkdir ~/Pictures
 #.config for configs
 #Pictures for wallpapers (after install you can change it in ~/.config/bspwm/bspwmrc/
 
-yay -Sy
+yay  --noconfirm -Sy
 
 #Updating repositories
 
                         read -p "Do you want to install pipewire(1) or pulseaudio(2)? 1/2   " AUDIO
                             case $AUDIO in
-                                        1 ) yay -S pipewire pipewire-pulse pavucontrol alsamixer; break;;
-                                        2 ) yay -S pulseaudio pavucontrol alsamixer; break;;
+                                        1 ) yay  --noconfirm -S pipewire pipewire-pulse pavucontrol alsamixer; break;;
+                                        2 ) yay  --noconfirm -S pulseaudio pavucontrol alsamixer; break;;
                                         * ) echo "Please answer 1/2.";;
                                                             esac
 
@@ -61,7 +62,7 @@ yay -Sy
 
                         read -p "Do you want to install terminal application like cmatrix?(3), no?(4)   " STUFF
                             case $STUFF in
-                                        3 ) yay -S cava cmatrix htop ; break;;
+                                        3 ) yay  --noconfirm -S cava cmatrix htop ; break;;
                                         4 ) echo ok; break;;
                                         * ) echo "Please answer 3/4.";;
                                                             esac
@@ -70,12 +71,12 @@ yay -Sy
 
                         read -p "Which File Manager would you like to install? pcmanfm (5), nautilus (6), dolphin (7), ranger (8), fff (9), nemo (10) or none (11)   " FM
                             case $FM in
-                                        5 ) yay -S pcmanfm ; break;;
-                                        6 ) yay -S nautilus; break;;
-                                        7 ) yay -S dolphin; break;;
-                                        8 ) yay -S ranger; break;;
-                                        9 ) yay -S fff; break;;
-                                        10 ) yay -S nemo; break;;
+                                        5 ) yay  --noconfirm -S pcmanfm ; break;;
+                                        6 ) yay  --noconfirm -S nautilus; break;;
+                                        7 ) yay  --noconfirm -S dolphin; break;;
+                                        8 ) yay  --noconfirm -S ranger; break;;
+                                        9 ) yay  --noconfirm -S fff; break;;
+                                        10 ) yay  --noconfirm -S nemo; break;;
                                         11 ) echo ok; break;;
                                         * ) echo "Please answer 5/6/7/8/9/10/11";;
                                                             esac
@@ -83,10 +84,10 @@ yay -Sy
 
                         read -p "Which terminal emulator would you like to install? alacritty (12), st (13), kitty (14), xfce4-termianl (15), none (16)   " TERMINAL
                             case $TERMINAL in
-                                        12 ) yay -S alacritty; break;;
-                                        13 ) yay -S st; break;;
-                                        14 ) yay -S kitty; break;;
-                                        15 ) yay -S xfce4-terminal; break;;
+                                        12 ) yay  --noconfirm -S alacritty; break;;
+                                        13 ) yay  --noconfirm -S st; break;;
+                                        14 ) yay  --noconfirm -S kitty; break;;
+                                        15 ) yay  --noconfirm -S xfce4-terminal; break;;
                                         16 ) echo ok; break;;
                                         * ) echo "Please answer 12/13/14/15/16";;
                                                             esac
@@ -95,7 +96,7 @@ yay -Sy
 
                         read -p "Would you like to install Nerd Fonts? This package is required to run pre-installed polybar config. Yes (17), No (18)   " NERD
                             case $NERD in
-                                        17 ) yay -S nerd-fonts-complete; break;;
+                                        17 ) yay  --noconfirm -S nerd-fonts-complete; break;;
                                         18 ) echo ok; break;;
                                         * ) echo "Please answer 17/18.";;
                                                             esac
@@ -104,12 +105,12 @@ yay -Sy
 
                         read -p "Would you like to install web browser? Librewolf (19), Firefox (20), Ungoogled Chromium (21), Vivaldi (22), Epiphany (23), Falkon (24), None (25)   " WEB
                             case $WEB in
-                                        19 ) yay -S librewolf; break;;
-                                        20 ) yay -S firefox; break;;
-                                        21 ) yay -S ungoogled-chromium; break;;
-                                        22 ) yay -S vivaldi; break;;
-					23 ) yay -S epiphany; break;;
-					24 ) yay -S falkon; break;;
+                                        19 ) yay  --noconfirm -S librewolf; break;;
+                                        20 ) yay  --noconfirm -S firefox; break;;
+                                        21 ) yay  --noconfirm -S ungoogled-chromium; break;;
+                                        22 ) yay  --noconfirm -S vivaldi; break;;
+					23 ) yay  --noconfirm -S epiphany; break;;
+					24 ) yay  --noconfirm -S falkon; break;;
 					25 ) echo ok; break;;
 					* ) echo "Please answer 19/20/21/22/23/24/25.";;
                                                             esac
